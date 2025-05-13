d6a30c6351d13d31879434c4e06508d31b0d6b26
LawrenceMadara <mark.madara@edu.sait.ca>
2025-05-13 10:31:15 -0600

    week 2 assignment completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Mark Madara</p>
+            <Link href="https://github.com/LawrenceMadara/cprg306-assignments">My repository</Link> 
+        </main>
+    );
+}
